91e788232ef31afddb0f6527363547d8328ce50d
hgene0929@gmail.com <hgene0929@gmail.com>
2023-12-29 17:09:42 +0900

    :tada: Initial commit.

---
+++ src/main/java/com/jyujyu/dayonetest/TestData.java
@@ -0,0 +1,13 @@
+package com.jyujyu.dayonetest;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 롬복 라이브러리가 정상적으로 동작하는지 확인하기 위해 롬복을 사용한 클래스 생성
+ */
+@Getter
+@Setter
+public class TestData {
+	private String name;
+}
---
+++ src/test/java/com/jyujyu/dayonetest/LombokTestDataTest.java
@@ -0,0 +1,19 @@
+package com.jyujyu.dayonetest;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * 롬복 라이브러리가 정상적으로 동작하는지 확인하기 위해 롬복 기능을 사용한 테스트 수행
+ */
+public class LombokTestDataTest {
+
+	@Test
+	public void testDataTest() {
+		TestData testData = new TestData();
+
+		testData.setName("jyujyu");
+
+		Assertions.assertEquals("jyujyu", testData.getName());
+	}
+}
