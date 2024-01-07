d14e2018bcfd15729b1c31737149b651b509f705
hgene0929@gmail.com <hgene0929@gmail.com>
2024-01-08 07:47:01 +0900

    합격자/불합격자 명단 가져오기 Stubbing 테스트

--- src/main/java/com/jyujyu/dayonetest/controller/response/ExamPassStudentResponse.java
+++ src/main/java/com/jyujyu/dayonetest/controller/response/ExamPassStudentResponse.java
@@ -1,9 +1,11 @@
 package com.jyujyu.dayonetest.controller.response;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 @Getter
+@EqualsAndHashCode // 객체간의 레퍼런스 비교시 동일한 값에 대해 true를 반환하도록 함(@Getter + @EqualsAndHashCode or @Data)
 @AllArgsConstructor
 public class ExamPassStudentResponse {
 	private final String studentName;
--- src/test/java/com/jyujyu/dayonetest/service/StudentScoreServiceMockTest.java
+++ src/test/java/com/jyujyu/dayonetest/service/StudentScoreServiceMockTest.java
@@ -1,9 +1,16 @@
 package com.jyujyu.dayonetest.service;
 
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import com.jyujyu.dayonetest.controller.response.ExamFailStudentResponse;
+import com.jyujyu.dayonetest.controller.response.ExamPassStudentResponse;
+import com.jyujyu.dayonetest.model.StudentFail;
+import com.jyujyu.dayonetest.model.StudentPass;
 import com.jyujyu.dayonetest.repository.StudentFailRepsitory;
 import com.jyujyu.dayonetest.repository.StudentPassRepository;
 import com.jyujyu.dayonetest.repository.StudentScoreRepository;
@@ -106,5 +113,79 @@ public class StudentScoreServiceMockTest {
 		Mockito.verify(studentScoreRepository, Mockito.times(1)).save(Mockito.any());
 		Mockito.verify(studentPassRepository, Mockito.times(0)).save(Mockito.any());
 		Mockito.verify(studentFailRepsitory, Mockito.times(1)).save(Mockito.any());
+	}
+
+	@Test
+	@DisplayName("합격자 명단 가져오기 검증")
+	public void getPassStudentListTest() {
+		// given
+		StudentScoreRepository studentScoreRepository = Mockito.mock(StudentScoreRepository.class);
+		StudentPassRepository studentPassRepository = Mockito.mock(StudentPassRepository.class);
+		StudentFailRepsitory studentFailRepsitory = Mockito.mock(StudentFailRepsitory.class);
+
+		StudentPass expectStudent1 = StudentPass.builder().id(1L).studentName("jyujyu").exam("testexam").avgScore(70.0).build();
+		StudentPass expectStudent2 = StudentPass.builder().id(2L).studentName("test").exam("testexam").avgScore(80.0).build();
+		StudentPass notExpectStudent = StudentPass.builder().id(3L).studentName("imnot").exam("secondexam").avgScore(90.0).build();
+
+		// Mockito.when().thenReturn() : when 파라미터의 동작이 호출되면, thenReturn() 파라미터의 내용이 무조건 수행됨
+		Mockito.when(studentPassRepository.findAll()).thenReturn(List.of(
+			expectStudent1,
+			expectStudent2,
+			notExpectStudent
+		));
+
+		StudentScoreService studentScoreService = new StudentScoreService(
+			studentScoreRepository,
+			studentPassRepository,
+			studentFailRepsitory
+		);
+
+		String givenTestExam = "testexam";
+
+		// when
+		var expectResponses = List.of(expectStudent1, expectStudent2).stream()
+			.map((pass) -> new ExamPassStudentResponse(pass.getStudentName(), pass.getAvgScore()))
+			.toList();
+		List<ExamPassStudentResponse> responses = studentScoreService.getPassStudentsList(givenTestExam);
+
+		// then
+		Assertions.assertIterableEquals(expectResponses, responses);
+	}
+
+	@Test
+	@DisplayName("불합격자 명단 가져오기 검증")
+	public void getFailStudnetsListTest() {
+		// given
+		StudentScoreRepository studentScoreRepository = Mockito.mock(StudentScoreRepository.class);
+		StudentPassRepository studentPassRepository = Mockito.mock(StudentPassRepository.class);
+		StudentFailRepsitory studentFailRepsitory = Mockito.mock(StudentFailRepsitory.class);
+
+		StudentFail notExpectStudent = StudentFail.builder().id(1L).studentName("jyujyu").exam("secondexam").avgScore(70.0).build();
+		StudentFail expectStudent1 = StudentFail.builder().id(2L).studentName("test").exam("testexam").avgScore(80.0).build();
+		StudentFail expectStudent2 = StudentFail.builder().id(3L).studentName("imnot").exam("testexam").avgScore(90.0).build();
+
+		// Mockito.when().thenReturn() : when 파라미터의 동작이 호출되면, thenReturn() 파라미터의 내용이 무조건 수행됨
+		Mockito.when(studentFailRepsitory.findAll()).thenReturn(List.of(
+			notExpectStudent,
+			expectStudent1,
+			expectStudent2
+		));
+
+		StudentScoreService studentScoreService = new StudentScoreService(
+			studentScoreRepository,
+			studentPassRepository,
+			studentFailRepsitory
+		);
+
+		String givenTestExam = "testexam";
+
+		// when
+		var expectFailList = List.of(expectStudent1, expectStudent2).stream()
+			.map((fail) -> new ExamFailStudentResponse(fail.getStudentName(), fail.getAvgScore()))
+			.toList();
+		List<ExamFailStudentResponse> responses = studentScoreService.getFailStudentsList(givenTestExam);
+
+		// then
+		Assertions.assertIterableEquals(expectFailList, responses);
 	}
 }
