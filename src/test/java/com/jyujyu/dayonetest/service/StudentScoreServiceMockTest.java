71fb740367445b7140c114f8b4ffdb49c0fe9dbb
hgene0929@gmail.com <hgene0929@gmail.com>
2024-01-08 07:24:22 +0900

    성적 저장 로직 검증 테스트 코드 작성

--- src/test/java/com/jyujyu/dayonetest/service/StudentScoreServiceMockTest.java
+++ src/test/java/com/jyujyu/dayonetest/service/StudentScoreServiceMockTest.java
@@ -20,6 +20,7 @@ public class StudentScoreServiceMockTest {
 			Mockito.mock(StudentPassRepository.class),
 			Mockito.mock(StudentFailRepsitory.class)
 		);
+
 		String givenStudentName = "jyujyu";
 		String givenExam = "testexam";
 		Integer givenKorScore = 80;
@@ -34,5 +35,76 @@ public class StudentScoreServiceMockTest {
 			givenEnglishScore,
 			givenMathScore
 		);
+	}
+
+	@Test
+	@DisplayName("성적 저장 로직 검증 / 60점 이상인 경우")
+	public void saveScoreMockTest() {
+		// given : 평균점수가 60점 이상인 경우
+		StudentScoreRepository studentScoreRepository = Mockito.mock(StudentScoreRepository.class);
+		StudentPassRepository studentPassRepository = Mockito.mock(StudentPassRepository.class);
+		StudentFailRepsitory studentFailRepsitory = Mockito.mock(StudentFailRepsitory.class);
+
+		StudentScoreService studentScoreService = new StudentScoreService(
+			studentScoreRepository,
+			studentPassRepository,
+			studentFailRepsitory
+		);
+
+		String givenStudentName = "jyujyu";
+		String givenExam = "testexam";
+		Integer givenKorScore = 80;
+		Integer givenEnglishScore = 100;
+		Integer givenMathScore = 60;
+
+		// when
+		studentScoreService.saveScore(
+			givenStudentName,
+			givenExam,
+			givenKorScore,
+			givenEnglishScore,
+			givenMathScore
+		);
+
+		// then
+		// Mockito.verify() : 1st 파라미터의 XX 메소드가 Mockito.times() 의 파라미터값만큼 호출되어야 함
+		Mockito.verify(studentScoreRepository, Mockito.times(1)).save(Mockito.any());
+		Mockito.verify(studentPassRepository, Mockito.times(1)).save(Mockito.any());
+		Mockito.verify(studentFailRepsitory, Mockito.times(0)).save(Mockito.any());
+	}
+
+	@Test
+	@DisplayName("성적 저장 로직 검증 / 60점 미만인 경우")
+	public void saveScoreMockTest2() {
+		// given : 평균점수가 60점 미만인 경우
+		StudentScoreRepository studentScoreRepository = Mockito.mock(StudentScoreRepository.class);
+		StudentPassRepository studentPassRepository = Mockito.mock(StudentPassRepository.class);
+		StudentFailRepsitory studentFailRepsitory = Mockito.mock(StudentFailRepsitory.class);
+
+		StudentScoreService studentScoreService = new StudentScoreService(
+			studentScoreRepository,
+			studentPassRepository,
+			studentFailRepsitory
+		);
+
+		String givenStudentName = "jyujyu";
+		String givenExam = "testexam";
+		Integer givenKorScore = 40;
+		Integer givenEnglishScore = 40;
+		Integer givenMathScore = 60;
+
+		// when
+		studentScoreService.saveScore(
+			givenStudentName,
+			givenExam,
+			givenKorScore,
+			givenEnglishScore,
+			givenMathScore
+		);
+
+		// then
+		Mockito.verify(studentScoreRepository, Mockito.times(1)).save(Mockito.any());
+		Mockito.verify(studentPassRepository, Mockito.times(0)).save(Mockito.any());
+		Mockito.verify(studentFailRepsitory, Mockito.times(1)).save(Mockito.any());
 	}
 }
