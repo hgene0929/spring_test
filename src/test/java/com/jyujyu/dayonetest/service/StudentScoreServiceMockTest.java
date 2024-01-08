2ff4e282ef08b0a5e3bb1a065d73738a7811031d
hgene0929@gmail.com <hgene0929@gmail.com>
2024-01-08 20:24:13 +0900

    Mockito 테스트 코드 리팩토링

--- src/test/java/com/jyujyu/dayonetest/service/StudentScoreServiceMockTest.java
+++ src/test/java/com/jyujyu/dayonetest/service/StudentScoreServiceMockTest.java
@@ -3,34 +3,46 @@ package com.jyujyu.dayonetest.service;
 import java.util.List;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 
-import com.jyujyu.dayonetest.MyCalculator;
 import com.jyujyu.dayonetest.controller.response.ExamFailStudentResponse;
 import com.jyujyu.dayonetest.controller.response.ExamPassStudentResponse;
 import com.jyujyu.dayonetest.model.StudentFail;
+import com.jyujyu.dayonetest.model.StudentFailFixture;
 import com.jyujyu.dayonetest.model.StudentPass;
+import com.jyujyu.dayonetest.model.StudentPassFixture;
 import com.jyujyu.dayonetest.model.StudentScore;
+import com.jyujyu.dayonetest.model.StudentScoreFixture;
+import com.jyujyu.dayonetest.model.StudentScoreTestDataBuilder;
 import com.jyujyu.dayonetest.repository.StudentFailRepsitory;
 import com.jyujyu.dayonetest.repository.StudentPassRepository;
 import com.jyujyu.dayonetest.repository.StudentScoreRepository;
 
 public class StudentScoreServiceMockTest {
 
+	// 리팩토링 : 테스트 메소드 내부 중복 로직 클래스 단위로 추출
+	private StudentScoreService studentScoreService;
+	private StudentScoreRepository studentScoreRepository;
+	private StudentPassRepository studentPassRepository;
+	private StudentFailRepsitory studentFailRepsitory;
+
+	@BeforeEach // 모든 테스트가 각각 실행되기 이전에 1번씩 수행됨
+	public void beforeEach() {
+		// Mockito.mock() : 서비스 클래스가 의존성주입 받아야하는 인터페이스의 Mock 객체 생성
+		studentScoreRepository = Mockito.mock(StudentScoreRepository.class);
+		studentPassRepository = Mockito.mock(StudentPassRepository.class);
+		studentFailRepsitory = Mockito.mock(StudentFailRepsitory.class);
+		studentScoreService = new StudentScoreService(studentScoreRepository, studentPassRepository, studentFailRepsitory);
+	}
+
 	@Test
 	@DisplayName("첫번째 Mock 테스트")
 	public void firstSaveScoreMockTest() {
 		// given
-		// Mockito.mock() : 서비스 클래스가 의존성주입 받아야하는 인터페이스의 Mock 객체 생성
-		StudentScoreService studentScoreService = new StudentScoreService(
-			Mockito.mock(StudentScoreRepository.class),
-			Mockito.mock(StudentPassRepository.class),
-			Mockito.mock(StudentFailRepsitory.class)
-		);
-
 		String givenStudentName = "jyujyu";
 		String givenExam = "testexam";
 		Integer givenKorScore = 80;
@@ -51,41 +63,9 @@ public class StudentScoreServiceMockTest {
 	@DisplayName("성적 저장 로직 검증 / 60점 이상인 경우")
 	public void saveScoreMockTest() {
 		// given : 평균점수가 60점 이상인 경우
-		StudentScoreRepository studentScoreRepository = Mockito.mock(StudentScoreRepository.class);
-		StudentPassRepository studentPassRepository = Mockito.mock(StudentPassRepository.class);
-		StudentFailRepsitory studentFailRepsitory = Mockito.mock(StudentFailRepsitory.class);
-
-		StudentScoreService studentScoreService = new StudentScoreService(
-			studentScoreRepository,
-			studentPassRepository,
-			studentFailRepsitory
-		);
-
-		String givenStudentName = "jyujyu";
-		String givenExam = "testexam";
-		Integer givenKorScore = 80;
-		Integer givenEnglishScore = 100;
-		Integer givenMathScore = 60;
-
-		StudentScore expectStudentScore = StudentScore.builder()
-			.studentName(givenStudentName)
-			.exam(givenExam)
-			.korScore(givenKorScore)
-			.englishScore(givenEnglishScore)
-			.mathScore(givenMathScore)
-			.build();
-		StudentPass expectStudentPass = StudentPass.builder()
-			.studentName(givenStudentName)
-			.exam(givenExam)
-			.avgScore(
-				(new MyCalculator(0.0))
-					.add(givenKorScore.doubleValue())
-					.add(givenEnglishScore.doubleValue())
-					.add(givenMathScore.doubleValue())
-					.divide(3.0)
-					.getResult()
-			)
-			.build();
+		// TestData Builder 패턴 : 빌더 패턴이기 때문에 오버라이딩하여 사용가능함
+		StudentScore expectStudentScore = StudentScoreTestDataBuilder.passed().build();
+		StudentPass expectStudentPass = StudentPassFixture.create(expectStudentScore);
 
 		// ArgumentCaptor : Mock 객체로 로직 확인시, 특정 메소드 호출할 때의 파라미터 타입을 지정 -> 내부에 접근가능하도록 해줌
 		ArgumentCaptor<StudentScore> studentScoreArgumentCaptor = ArgumentCaptor.forClass(StudentScore.class);
@@ -93,11 +73,11 @@ public class StudentScoreServiceMockTest {
 
 		// when
 		studentScoreService.saveScore(
-			givenStudentName,
-			givenExam,
-			givenKorScore,
-			givenEnglishScore,
-			givenMathScore
+			expectStudentScore.getStudentName(),
+			expectStudentScore.getExam(),
+			expectStudentScore.getKorScore(),
+			expectStudentScore.getEnglishScore(),
+			expectStudentScore.getMathScore()
 		);
 
 		// then
@@ -123,52 +103,20 @@ public class StudentScoreServiceMockTest {
 	@DisplayName("성적 저장 로직 검증 / 60점 미만인 경우")
 	public void saveScoreMockTest2() {
 		// given : 평균점수가 60점 미만인 경우
-		StudentScoreRepository studentScoreRepository = Mockito.mock(StudentScoreRepository.class);
-		StudentPassRepository studentPassRepository = Mockito.mock(StudentPassRepository.class);
-		StudentFailRepsitory studentFailRepsitory = Mockito.mock(StudentFailRepsitory.class);
-
-		StudentScoreService studentScoreService = new StudentScoreService(
-			studentScoreRepository,
-			studentPassRepository,
-			studentFailRepsitory
-		);
-
-		String givenStudentName = "jyujyu";
-		String givenExam = "testexam";
-		Integer givenKorScore = 40;
-		Integer givenEnglishScore = 40;
-		Integer givenMathScore = 60;
-
-		StudentScore expectStudentScore = StudentScore.builder()
-			.studentName(givenStudentName)
-			.exam(givenExam)
-			.korScore(givenKorScore)
-			.englishScore(givenEnglishScore)
-			.mathScore(givenMathScore)
-			.build();
-		StudentFail expectStudentFail = StudentFail.builder()
-			.studentName(givenStudentName)
-			.exam(givenExam)
-			.avgScore(
-				(new MyCalculator(0.0))
-					.add(givenKorScore.doubleValue())
-					.add(givenEnglishScore.doubleValue())
-					.add(givenMathScore.doubleValue())
-					.divide(3.0)
-					.getResult()
-			)
-			.build();
+		// TestData Fixture 패턴 : 오버라이딩 불가, 고정된 값
+		StudentScore expectStudentScore = StudentScoreFixture.failed();
+		StudentFail expectStudentFail = StudentFailFixture.create(expectStudentScore);
 
 		ArgumentCaptor<StudentScore> studentScoreArgumentCaptor = ArgumentCaptor.forClass(StudentScore.class);
 		ArgumentCaptor<StudentFail> studentFailArgumentCaptor = ArgumentCaptor.forClass(StudentFail.class);
 
 		// when
 		studentScoreService.saveScore(
-			givenStudentName,
-			givenExam,
-			givenKorScore,
-			givenEnglishScore,
-			givenMathScore
+			expectStudentScore.getStudentName(),
+			expectStudentScore.getExam(),
+			expectStudentScore.getKorScore(),
+			expectStudentScore.getEnglishScore(),
+			expectStudentScore.getMathScore()
 		);
 
 		// then
@@ -181,6 +129,7 @@ public class StudentScoreServiceMockTest {
 		Assertions.assertEquals(expectStudentScore.getMathScore(), capturedStudentScore.getMathScore());
 
 		Mockito.verify(studentPassRepository, Mockito.times(0)).save(Mockito.any());
+
 		Mockito.verify(studentFailRepsitory, Mockito.times(1)).save(studentFailArgumentCaptor.capture());
 		StudentFail capturedStudentFail = studentFailArgumentCaptor.getValue();
 		Assertions.assertEquals(expectStudentFail.getStudentName(), capturedStudentFail.getStudentName());
@@ -192,13 +141,10 @@ public class StudentScoreServiceMockTest {
 	@DisplayName("합격자 명단 가져오기 검증")
 	public void getPassStudentListTest() {
 		// given
-		StudentScoreRepository studentScoreRepository = Mockito.mock(StudentScoreRepository.class);
-		StudentPassRepository studentPassRepository = Mockito.mock(StudentPassRepository.class);
-		StudentFailRepsitory studentFailRepsitory = Mockito.mock(StudentFailRepsitory.class);
-
-		StudentPass expectStudent1 = StudentPass.builder().id(1L).studentName("jyujyu").exam("testexam").avgScore(70.0).build();
-		StudentPass expectStudent2 = StudentPass.builder().id(2L).studentName("test").exam("testexam").avgScore(80.0).build();
-		StudentPass notExpectStudent = StudentPass.builder().id(3L).studentName("imnot").exam("secondexam").avgScore(90.0).build();
+		String givenTestExam = "testexam";
+		StudentPass expectStudent1 = StudentPassFixture.create("jyujyu", givenTestExam);
+		StudentPass expectStudent2 = StudentPassFixture.create("testName", givenTestExam);
+		StudentPass notExpectStudent = StudentPassFixture.create("anotherStudent", "anotherExam");
 
 		// Mockito.when().thenReturn() : when 파라미터의 동작이 호출되면, thenReturn() 파라미터의 내용이 무조건 수행됨
 		Mockito.when(studentPassRepository.findAll()).thenReturn(List.of(
@@ -212,8 +158,6 @@ public class StudentScoreServiceMockTest {
 			studentPassRepository,
 			studentFailRepsitory
 		);
-
-		String givenTestExam = "testexam";
 
 		// when
 		var expectResponses = List.of(expectStudent1, expectStudent2).stream()
@@ -229,13 +173,10 @@ public class StudentScoreServiceMockTest {
 	@DisplayName("불합격자 명단 가져오기 검증")
 	public void getFailStudnetsListTest() {
 		// given
-		StudentScoreRepository studentScoreRepository = Mockito.mock(StudentScoreRepository.class);
-		StudentPassRepository studentPassRepository = Mockito.mock(StudentPassRepository.class);
-		StudentFailRepsitory studentFailRepsitory = Mockito.mock(StudentFailRepsitory.class);
-
-		StudentFail notExpectStudent = StudentFail.builder().id(1L).studentName("jyujyu").exam("secondexam").avgScore(70.0).build();
-		StudentFail expectStudent1 = StudentFail.builder().id(2L).studentName("test").exam("testexam").avgScore(80.0).build();
-		StudentFail expectStudent2 = StudentFail.builder().id(3L).studentName("imnot").exam("testexam").avgScore(90.0).build();
+		String givenTestExam = "testexam";
+		StudentFail notExpectStudent = StudentFailFixture.create("jyujyu", "anotherTest");
+		StudentFail expectStudent1 = StudentFailFixture.create("testName", givenTestExam);
+		StudentFail expectStudent2 = StudentFailFixture.create("testName2", givenTestExam);
 
 		// Mockito.when().thenReturn() : when 파라미터의 동작이 호출되면, thenReturn() 파라미터의 내용이 무조건 수행됨
 		Mockito.when(studentFailRepsitory.findAll()).thenReturn(List.of(
@@ -249,8 +190,6 @@ public class StudentScoreServiceMockTest {
 			studentPassRepository,
 			studentFailRepsitory
 		);
-
-		String givenTestExam = "testexam";
 
 		// when
 		var expectFailList = List.of(expectStudent1, expectStudent2).stream()
